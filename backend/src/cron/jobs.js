caebb2ba8cdaa8591562c79d86d8a40b0fe33976
ravinverma547 <ravinverma547@gmail.com>
2026-03-23 11:05:49 +0530

    Commit deletions and update code

--- backend/src/cron/jobs.js
+++
@@ -1,8 +0,0 @@
-const cron = require('node-cron');
-// Placeholder for cron jobs
-
-cron.schedule('0 0 * * *', () => {
-    console.log('Running daily maintenance tasks...');
-});
-
-module.exports = cron;
